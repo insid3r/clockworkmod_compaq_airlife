69094dea99d97731af2525218097787acbf241cc
insid3r <t.insider@gmail.com>
2011-07-25 14:23:10 +0100

    first commit

---
+++ BoardConfig.mk
@@ -0,0 +1,29 @@
+USE_CAMERA_STUB := true
+
+# inherit from the proprietary version
+-include vendor/compaq/airlife/BoardConfigVendor.mk
+
+TARGET_NO_BOOTLOADER := true
+TARGET_BOARD_PLATFORM := unknown
+TARGET_CPU_ABI := armeabi
+TARGET_BOOTLOADER_BOARD_NAME := airlife
+
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom
+BOARD_KERNEL_BASE := 0x20000000
+BOARD_PAGE_SIZE := 0x00001000
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_NAND_PAGE_SIZE := 4096 -s 128
+
+# fix this up by examining /proc/mtd on a running device
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00500000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00500000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x08000000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x08000000
+#BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_FLASH_BLOCK_SIZE := 4096
+
+TARGET_PREBUILT_KERNEL := device/compaq/airlife/kernel
+
+#BOARD_HAS_NO_SELECT_BUTTON := true
+# Use this flag if the board has a ext4 partition larger than 2gb
+#BOARD_HAS_LARGE_FILESYSTEM := true
